5f7d4dc00057987fe854ad126f969849dd7aa07d
Aish <aishwaryasharmapari@gmail.com>
2026-08-24 10:29:27 +0530

    Code to print the value from 1 to n where n  is inputted by the user

---
+++ print_1ton.c
@@ -0,0 +1,16 @@
+
+
+#include <stdio.h>
+
+int main()
+{  
+    int n,i;
+    printf("Input the value of n");
+    scanf("%d",&n);
+    for (i=1;i<=n;i++)
+    {
+        printf("\nThe value from 1 to n is : %d",i);
+    }
+
+    return 0;
+}
