8c73949ae3776daf19dbaa7ca8493074ed843b6f
Aish <aishwaryasharmapari@gmail.com>
2026-08-30 23:47:49 +0530

    Write a program to print the product of even numbers from 1 to n

---
+++ product_even.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, p = 1;
+    printf("Input the value of n");
+    scanf("%d", &n);
+
+    for(i = 2; i <= n; i = i + 2)
+    {
+        p = p * i;
+    }
+
+    printf("%d", p);
+
+    return 0;
+}
